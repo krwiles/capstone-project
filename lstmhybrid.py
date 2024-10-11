508a6fa08456997068cc98e7ba5df24d9d2c0cca
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-10-11 14:15:24 -0400

    Added UI validation and updated model

--- lstmhybrid.py
+++ lstmhybrid.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class LSTMHybrid(nn.Module):
-    def __init__(self, input_size=11, hidden_size=128, num_layers=2, output_size=1):
+    def __init__(self, input_size=11, hidden_size=128, num_layers=1, output_size=1):
         super().__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers    
@@ -14,21 +14,16 @@ class LSTMHybrid(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, output_size)  # Final output layer
+            nn.Linear(hidden_size, output_size)
         )
 
     def forward(self, x):
         hidden_states = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
         cell_states = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
         
-        out, _ = self.lstm(x, (hidden_states, cell_states)) # pass through LSTM layers
-        out = out[:, -1, :]  # Get the output of the last time step
+        out, _ = self.lstm(x, (hidden_states, cell_states)) 
+        out = out[:, -1, :] 
         
-        # pass through fully connected layers
         out = self.fc_layers(out)
         
         return out
